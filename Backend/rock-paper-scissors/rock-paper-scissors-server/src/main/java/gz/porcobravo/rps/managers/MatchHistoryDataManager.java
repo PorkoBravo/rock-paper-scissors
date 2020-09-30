714097ce4cb6dc365c08ec71d1062069b35a3b5a
dario.beiro <dario.beiro@evelb.es>
2020-09-30 20:42:40 +0200

    #history: red on match history data service

--- Backend/rock-paper-scissors/rock-paper-scissors-server/src/main/java/gz/porcobravo/rps/managers/MatchHistoryDataManager.java
+++ Backend/rock-paper-scissors/rock-paper-scissors-server/src/main/java/gz/porcobravo/rps/managers/MatchHistoryDataManager.java
@@ -1,11 +1,13 @@
 package gz.porcobravo.rps.managers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import gz.porcobravo.rps.data.MatchHistoryData;
 import gz.porcobravo.rps.data.MatchHistoryDatabase;
 import gz.porcobravo.rps.services.MatchHistoryDataService;
 
+@Service
 public class MatchHistoryDataManager implements MatchHistoryDataService {
 	
 	@Autowired
