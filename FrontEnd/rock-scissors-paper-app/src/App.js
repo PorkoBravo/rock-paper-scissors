89b43c15d3f3c9f58643b871cb4ce9325ba7b90a
dario.beiro <dario.beiro@evelb.es>
2020-10-01 20:25:01 +0200

    #playmatch: addedHistory

--- FrontEnd/rock-scissors-paper-app/src/App.js
+++ FrontEnd/rock-scissors-paper-app/src/App.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import './App.css';
 import MatchTable from './components/matchTable';
+import MatchHistory from './components/matchHistory';
 
-function App() {
-  return (
-    <div className="App">
-      <MatchTable />
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super()
+    this.state = { viewMatchHistory: false }
+  }
+
+  alternateMatchHistory() {
+    let _viewMatchHistory = !this.state.viewMatchHistory
+    this.setState((state) => {
+      return {viewMatchHistory: _viewMatchHistory}
+    });
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <button class="secondary-button" id="alternate" onClick={() => this.alternateMatchHistory()}>Alternate History View</button>
+        {!this.state.viewMatchHistory && <MatchTable />}
+        {this.state.viewMatchHistory && <MatchHistory />}
+      </div>
+    );
+  }
 }
+
+
+
 
 export default App;
--- FrontEnd/rock-scissors-paper-app/src/api/api.js
+++ FrontEnd/rock-scissors-paper-app/src/api/api.js
@@ -4,3 +4,7 @@ import axios from 'axios'
 export function playNewRound() {
     return axios.post('http://localhost:8085/match');
 }
+
+export function getTotalMatchHistory() {
+    return axios.get('http://localhost:8085/history');
+}
