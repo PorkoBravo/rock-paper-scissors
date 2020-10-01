6c4a7eb943e9c8b80f0d6919823bf3edb2b319cb
dario.beiro <dario.beiro@evelb.es>
2020-10-01 18:43:45 +0200

    #Added cors annotation on backend controller

--- Backend/rock-paper-scissors/rock-paper-scissors-server/src/main/java/gz/porcobravo/rps/controllers/MatchController.java
+++ Backend/rock-paper-scissors/rock-paper-scissors-server/src/main/java/gz/porcobravo/rps/controllers/MatchController.java
@@ -1,6 +1,7 @@
 package gz.porcobravo.rps.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import gz.porcobravo.dtos.MatchResult;
 import gz.porcobravo.rps.services.MatchService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping(path = "/match")
 public class MatchController {
 	
